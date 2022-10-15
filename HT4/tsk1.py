869d63d93f634d73748ab96e91a3d08296f44009
Mykola Yustyk <nikola.justik@gmail.com>
2022-10-15 13:19:22 +0300

    add HT4

---
+++ HT4/tsk1.py
@@ -0,0 +1,20 @@
+'''
+1. Написати функцiю season, яка приймає один аргумент (номер мiсяця вiд 1 до 12) та яка буде повертати
+пору року, до якої цей мiсяць належить (зима, весна, лiто або осiнь).
+У випадку некоректного введеного значення - виводити відповідне повідомлення.
+'''
+
+def season(month):
+    if month in [1, 2, 12]:
+        return 'winter'
+    elif month in [3, 4, 5]:
+        return 'spring'
+    elif month in [6, 7, 8]:
+        return 'summer'
+    elif month in [9, 10, 11]:
+        return 'autumn'
+    else:
+        return 'Illegal number of month '
+    
+input_month = int(input('Input number of month: '))
+print(f'It is {season(input_month)} month')
