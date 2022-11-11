7cd8ef557135d8cde2cb8264da86821bbf1a4df0
MykolaYustyk <94138609+MykolaYustyk@users.noreply.github.com>
2022-11-11 16:12:48 +0200

    rewrite 12.py

---
+++ HT10/12.py
@@ -0,0 +1,30 @@
+import math
+
+limits = {1000:4, 500: 1, 200:4, 100: 0, 50: 1, 20: 1, 10: 5}
+
+def collect(amount, nominals):
+    if amount == 0 :
+        return  dict()
+    if not nominals:
+        return
+    
+    current_nominal = nominals[0]
+    avaible_notes = limits[current_nominal]
+    notes_needed = math.floor(amount / current_nominal)
+    number_of_notes = min(avaible_notes, notes_needed)
+   
+    for i in range(number_of_notes, -1, -1):
+        result = collect(amount - i * current_nominal, nominals[1:])    
+    
+        if result  is not None:
+            if i:
+                result[current_nominal] = number_of_notes
+                return result
+            else:
+                return result
+    
+def i_want_to_get(amount_required, limits):
+    nominals = sorted(limits.keys(), reverse=1)
+    return collect(amount_required, nominals)
+    
+print(i_want_to_get(1170, limits))
