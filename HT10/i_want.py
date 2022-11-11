c6c7b90ca8b818abc73f6c69aba0cf2409ed2a31
MykolaYustyk <nikola.justik@gmail.com>
2022-11-11 10:42:03 +0200

    rewrite I_want

--- HT10/i_want.py
+++ HT10/i_want.py
@@ -1,6 +1,6 @@
 import math
 
-limits = {1000: 5, 500: 1, 200:4, 100: 0, 50: 1, 20: 1, 10: 5}
+limits = {1000: 5, 500: 1, 200:4, 100: 1, 50: 4, 20: 6, 10: 0}
 
 def collect(amount, nominals):
     if amount == 0 :
@@ -15,7 +15,7 @@ def collect(amount, nominals):
    
     for i in range(number_of_notes, -1, -1):
         nominals = nominals[1:]    
-        result = collect(amount - number_of_notes * current_nominal, nominals)
+        result = collect(amount - i * current_nominal, nominals)
     
         if result is not None:
             if number_of_notes:
@@ -26,4 +26,4 @@ def i_want_to_get(amount_required, limits):
     nominals = sorted(limits.keys(), reverse=1)
     return collect(amount_required, nominals)
     
-print(i_want_to_get(320, limits))
+print(i_want_to_get(1170, limits))
