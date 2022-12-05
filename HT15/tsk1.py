8bc84b9253cc999504ab85b4b9d78ecf7abcebcb
Mykola Yustyk <nikola.justik@gmail.com>
2022-12-05 19:24:14 +0200

    rewrite task1

--- HT15/tsk1.py
+++ HT15/tsk1.py
@@ -9,7 +9,8 @@ from bs4 import BeautifulSoup as bs
 
 BASE_URL = 'https://www.expireddomains.net/namecheap-auction-domains/'
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36 OPR/92.0.0.0'
-responce = requests.get(BASE_URL, timeout=(10, 0.1))
-soup = bs(responce.contetnt, 'lxml')
+responce = requests.get(BASE_URL, headers={'User-Agent': user_agent})
+print(responce.headers)
+soup = bs(responce.content, 'lxml')
 print(soup)
     
