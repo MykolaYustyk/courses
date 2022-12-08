3c3f45d1bcee95cc5318462c6ab40aa9ca2ba9bc
MykolaYustyk <nikola.justik@gmail.com>
2022-12-08 08:58:09 +0200

    rewrite tsk1 with classes

--- HT15/tsk1.py
+++ HT15/tsk1.py
@@ -16,34 +16,37 @@ user_agent = {
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0\
                  Safari/537.36 OPR/92.0.0.0'
 }
-result = []
 
 
-def parse_first_page(BASE_URL):
-    page = requests.get(BASE_URL, headers=user_agent).content
-    page_soup = bs(page, 'lxml')
-    domains = page_soup.select('.base1 tbody tr')
-    for domain in domains:
-        result.append(tuple(domain.text.split('\n')[1:-1]))
-    return result
+class SiteParser:
+    result = []
 
+    def __init__(self, url):
+        self.url = url
 
-def parse_next_pages(BASE_URL):
-    for page_number in range(2, 11):
-        home_page = BASE_URL + f'?start={25 * (page_number - 1)}#listing'
-        print(f'Parse page # {page_number}')
-        result.extend(parse_first_page(home_page))
-    return result
+    def parse_single_page(self):
+        page = requests.get(self.url, headers=user_agent).content
+        page_soup = bs(page, 'lxml')
+        domains = page_soup.select('.base1 tbody tr')
+        for domain in domains:
+            self.result.append(tuple(domain.text.split('\n')[1:-1]))
+        return self.result
 
+    def parse_pages(self):
+        for page_number in range(13):
+            self.url = BASE_URL + f'?start={page_number * 25}#listing'
+            print(f'Parse page # {page_number + 1}')
+            self.parse_single_page()
+        return self.result
 
-def write_info_to_csv(result):
-    with open('domains.csv', 'w', newline='', encoding='UTF-8') as file:
-        writer = csv.writer(file, delimiter=';')
-        writer.writerow(FILE_FIELDS)
-        writer.writerows(result)
+    def write_info_to_csv(self):
+        with open('domains.csv', 'w', newline='', encoding='UTF-8') as file:
+            writer = csv.writer(file, delimiter=';')
+            writer.writerow(FILE_FIELDS)
+            writer.writerows(self.result)
 
 
 if __name__ == "__main__":
-    parse_first_page(BASE_URL)
-    parse_next_pages(BASE_URL)
-    write_info_to_csv(result)
+    parser = SiteParser(BASE_URL)
+    parser.parse_pages()
+    parser.write_info_to_csv()
