d81d8494c9a85615c6ca684091f6ea984228ec87
Mykola Yustyk <nikola.justik@gmail.com>
2023-01-03 20:40:31 +0200

    rewrite tsk1 with try

--- HT17/tsk1.py
+++ HT17/tsk1.py
@@ -73,20 +73,25 @@ def preview_robot(driver):
 
 def save_robot_picture(driver, robot):
     robot_picture = driver.find_element(By.CSS_SELECTOR, 'div[id="robot-preview-image"]')
-    wait = WebDriverWait(driver, 20)
-    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'div[id="robot-preview-image"]')))
+    wait = WebDriverWait(driver, 10)
+    wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, 'div[id="robot-preview-image"]')))
     file_name = f'robot{robot["Order number"]}.png'
     robot_picture.screenshot(f'output\\{file_name}')
     return file_name
 
 
-def click_button_order(driver):
+def click_button_order(driver, robot):
     while True:
-        button_order = WebDriverWait(driver, 20)
-        button_order.until(EC.element_to_be_clickable((By.CLASS_NAME, 'btn-primary'))).click()
-        receipt = WebDriverWait(driver, 20)
-        if receipt.until(EC.visibility_of_element_located((By.CLASS_NAME, 'alert-success'))).is_displayed():
-            break
+        try:
+            button_order = WebDriverWait(driver, 2)
+            button_order.until(EC.element_to_be_clickable((By.CLASS_NAME, 'btn-primary'))).click()
+            receipt = WebDriverWait(driver, 5)
+            check_receipt = receipt.until(EC.visibility_of_element_located((By.CLASS_NAME, 'alert-success'))).is_displayed()
+            if check_receipt:
+                break
+        except:
+            continue
+
 
 
 def rename_robot_file(driver, file):
@@ -101,7 +106,7 @@ def order_another_robot(driver):
 
 def main():
     driver = webdriver.Chrome(ChromeDriverManager().install())
-    driver.implicitly_wait(10)
+    driver.implicitly_wait(5)
     create_output_folder()
     list_of_robots = read_csv(driver)
     click_order_your_robot(driver)
@@ -109,7 +114,7 @@ def main():
         set_robot_parts(driver, robot)
         preview_robot(driver)
         current_robot = save_robot_picture(driver, robot)
-        click_button_order(driver)
+        click_button_order(driver, robot)
         rename_robot_file(driver, current_robot)
         order_another_robot(driver)
     driver.quit()
